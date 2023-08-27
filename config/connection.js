110ff74e23820021102bf56955e1de2f5d87c4dd
Eissa Purnell <eissamonet@gmail.com>
2023-08-26 20:30:37 -0700

    added schema data

---
+++ config/connection.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql2');
+
+require('dotenv').config();
+
+const connection = mysql.createConnection({
+  host: '127.0.0.1',
+  port: 3306,
+  user: 'root',
+  password: process.env.MYSQL_PASSWORD,
+  database: 'employees'
+});
+
+module.exports = connection;
