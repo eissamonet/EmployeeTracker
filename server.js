9183feb14e1f8d81174e9166fe2248dab923651c
Eissa Purnell <eissamonet@gmail.com>
2023-08-27 23:38:19 -0500

    added user prompt of qiestions

--- server.js
+++ server.js
@@ -23,3 +23,64 @@ const db = mysql.createConnection(
   console.log(`Connected to the employee_db database.`)
 );
 
+//prompt user with a list of options
+function promptUser() {
+  return inquire.prompt([
+    {
+      type: 'list',
+      name: 'options',
+      message: 'What would you like to do?',
+      choices: [
+        'View all departments',
+        'View all roles',
+        'View all employees',
+        'Add a department',
+        'Add a role',
+        'Add an employee',
+        'Update an employee role',
+        'Exit'
+      ]
+    }
+  ])
+    .then((answers) => {
+      switch (answers.options) {
+        case 'View all departments':
+          viewDepartments();
+          break;
+        case 'View all roles':
+          viewRoles();
+          break;
+        case 'View all employees':
+          viewEmployees();
+          break;
+        case 'Add a department':
+          addDepartment();
+          break;
+        case 'Add a role':
+          addRole();
+          break;
+        case 'Add an employee':
+          addEmployee();
+          break;
+        case 'Update an employee role':
+          updateEmployee();
+          break;
+        case 'Update an employee manager':
+          updateManager();
+          break;
+        case 'Delete a department':
+          deleteDepartment();
+          break;
+        case 'Delete a role':
+          deleteRole();
+          break;
+        case 'Delete an employee':
+          deleteEmployee();
+          break;
+        case 'Exit':
+          db.end();
+          break;
+      }
+    })
+}
+
